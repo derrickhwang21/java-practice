698fa2d9d0cd4b20b0a425b5908f5548b05604d6
dhwang21 <hwang.derrick@gmail.com>
2018-12-03 00:24:57 -0800

    created linter class

--- linter/src/test/java/LinterTest.java
+++ linter/src/test/java/LinterTest.java
@@ -4,9 +4,9 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class AppTest {
+public class LinterTest {
     @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting("hello"));
+        Linter classUnderTest = new Linter();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
 }
